ac7441064c75e9502e5e23c796eea5eb3f3932f1
mi7er03gf@code.edu.az <mi7er03gf@code.edu.az>
2023-12-21 16:10:56 +0400

    task

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,6 +3,11 @@ import './navbar.scss'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+    function handleMenu() {
+        document.querySelector('.resmenu').classList.toggle('menutoggle')  
+    }
+
     return (
         <>
             <nav>
@@ -14,6 +19,12 @@ function Navbar() {
                     <p>Sign Up</p>
                 </div>
                 <div className="cart">
+                    <i onClick={handleMenu} className="fa-solid fa-bars"></i>
+                    <div className="resmenu">
+                        <Link to={"/"}><p>Home</p></Link>
+                        <Link to={"/contact"}><p>Contact</p></Link>
+                        <Link to={"/about"}><p>About</p></Link>
+                    </div>
                     <input type="text" placeholder='What are you looking for?' />
                     <i className="fa-regular fa-heart"></i>
                     <Link to={"/cart"}><i className="fa-solid fa-cart-shopping"></i></Link>
